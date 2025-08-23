19d8e80096d3a30b2829264615b3059683688d4c
gutiluis <gutierrez.luis@icloud.com>
2025-08-22 21:45:50 -0700

    new files

---
+++ models.py
@@ -0,0 +1,27 @@
+#!/bin/python
+from sqlalchemy import (create_engine, Column,
+                        Integer, String, Date)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+engine = create_engine("sqlite:///inventory.db", echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    product_id = Column(Integer, primary_key=True)
+    product_name = Column("Name", String)
+    product_quantity = Column("Quantity", Integer)
+    product_price = Column("Price", Integer)
+    date_updated = Column("Last_update", Date)
+
+    def __repr__(self):
+        return f"Name: {self.product_name}, Quantity: {self.product_quantity}, Price: {self.price}, Last_update: {self.date_updated}"
+
